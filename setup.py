5650a57ff90a14cfedee21dc6523282a9d92a1da
mordy <imky171@gmail.com>
2021-11-02 20:34:29 -0400

    added setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+from mathinator import version
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Mathinator",
+    version=version,
+    author="Israel Waldner",
+    author_email="imky171@gmail.com",
+    description="A command line tool to do hard math quickly",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/pypa/sampleproject",
+    # project_urls={
+        # "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    # },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+		install_requires = [
+			'click'
+		],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
